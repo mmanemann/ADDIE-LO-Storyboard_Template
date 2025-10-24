ebadca5ce53957a5a0346052ed7c5c007592f3d7
mmanemann <mmanemann@greatriverlearning.com>
2025-10-24 12:26:24 -0500

    resolve merge conflicts during rebase
    resolve initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Monitor, Tablet, Smartphone, ArrowRight } from 'lucide-react';
 
@@ -288,3 +289,30 @@ const DeviceMatrix = () => {
 };
 
 export default DeviceMatrix;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 9d934f5 (Initialize project using Create React App)
